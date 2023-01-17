39c3f570d5d07d1029ee25cc308455ee5d41f060
Omar Karame <omarkarmae21@gmail.com>
2023-01-17 17:15:51 +0000

    Updated some styling in the interests page

--- src/components/IntroSection/IntroSection.jsx
+++ src/components/IntroSection/IntroSection.jsx
@@ -7,18 +7,26 @@ const IntroSection = () => {
 
   return (
       <div className='intro-section'>
-        <img src={VexRobotics} alt="Vex Robotics Logo" className='vex-robotics'/>
-        <h4 className='vex-robotics-paragraph'>
-            The first time I was introduced to programming was during my final year at school in Lebanon. During that time, I was selected from my class to represent my school (LWIS - Universal School of Lebanon) in the national Vex Robotics competetion. My role in the team was to program the robots movement which could alternate between moving autonomously or by the controller’s input.
-        </h4>
-        <img src={QueenMary} alt="Queen Mary University of London Logo" className='queen-mary'/>
-        <h4 className='queen-mary-paragraph'>
-            After graduating from school, I moved to London to study computer science at Queen Mary University of London. During my time at university, I learned a lot computing and programming theory. This degree helped enhance my knowledge on computers and provided me with an introduction into programming.
-        </h4>
-        <img src={Nology} alt="_nology Logo" className='nology' />
-        <h4 className='nology-paragraph'>
-            After graduating from school, I moved to London to study computer science at Queen Mary University of London. During my time at university, I learned a lot computing and programming theory. This degree helped enhance my knowledge on computers and provided me with an introduction into programming.
-        </h4>
+        <div>
+          <div className='content-section'>
+            <img src={VexRobotics} alt="Vex Robotics Logo" className='vex-robotics'/>
+            <h4 className='vex-robotics-paragraph'>
+                The first time I was introduced to programming was during my final year at school in Lebanon. During that time, I was selected from my class to represent my school (LWIS - Universal School of Lebanon) in the national Vex Robotics competetion. My role in the team was to program the robots movement which could alternate between moving autonomously or by the controller’s input.
+            </h4>
+          </div>
+          <div className='content-section'>
+            <img src={QueenMary} alt="Queen Mary University of London Logo" className='queen-mary'/>
+            <h4 className='queen-mary-paragraph'>
+                After graduating from school, I moved to London to study computer science at Queen Mary University of London. During my time at university, I learned a lot computing and programming theory. This degree helped enhance my knowledge on computers and provided me with an introduction into programming.
+            </h4>
+          </div>
+          <div className='content-section'>
+            <img src={Nology} alt="_nology Logo" className='nology' />
+            <h4 className='nology-paragraph'>
+                After graduating from school, I moved to London to study computer science at Queen Mary University of London. During my time at university, I learned a lot computing and programming theory. This degree helped enhance my knowledge on computers and provided me with an introduction into programming.
+            </h4>
+          </div>
+        </div>
       </div>
   )
 }
